79fc735493bec37db82a8d2cc8ac021e3ab52f4f
othaderek <otha.hernandez@gmail.com>
2020-02-11 15:32:30 -0500

    Added Container component.

---
+++ src/Container.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import App from './App'
+// import { connect } from 'react-redux'
+
+export class Container extends Component {
+    render() {
+        return (
+            <div className = 'container'>
+                Hello from container
+            </div>
+        )
+    }
+}
+
+export default Container;
